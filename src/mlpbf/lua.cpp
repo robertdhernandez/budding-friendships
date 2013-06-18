0b8b045a6903a33bec88c5553893b6852fe80aa1
Bobby Hernandez <killerjaguar26@gmail.com>
2013-06-18 15:00:58 -0400

    Fixed makefile and compiles proper now

--- src/mlpbf/lua.cpp
+++ src/mlpbf/lua.cpp
@@ -11,15 +11,16 @@ namespace lua
 int lua_showText( lua_State * l )
 {
 	const char * ctext = luaL_checkstring( l, 1 );
-	const char * cspeaker = luaL_optstring( l, 2, NULL, NULL );
+	const char * cspeaker = luaL_optstring( l, 2, NULL );
 	
 	bf::showText( ctext, std::string( cspeaker != NULL ? cspeaker : "" ) );
+	return 0;
 }
 
-static const struct luaL_reg testLib[] = 
+static const struct luaL_Reg testLib[] = 
 {
 	{ "showText", lua_showText },
-	{ NULL, NULL }
+	{ NULL, NULL },
 };
 
 /***************************************************************************/
@@ -27,7 +28,7 @@ static const struct luaL_reg testLib[] =
 lua_State * newState()
 {
 	// create lua state
-	lua_State * l = luaL_newState();
+	lua_State * l = luaL_newstate();
 	luaL_openlibs( l );
 	
 	// register custom libraries

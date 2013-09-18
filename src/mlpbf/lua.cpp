b0d011379433e3556d2642a5fb8920a2f2763e92
Bobby Hernandez <killerjaguar26@gmail.com>
2013-09-18 13:10:52 -0400

    Added lua function to check if a key is pressed

--- src/mlpbf/lua.cpp
+++ src/mlpbf/lua.cpp
@@ -17,6 +17,7 @@
 #include <SFML/Graphics/RenderTarget.hpp>
 #include <SFML/Graphics/Sprite.hpp>
 #include <SFML/Graphics/Text.hpp>
+#include <SFML/Window/Keyboard.hpp>
 #include <SFML/System/Clock.hpp>
 
 namespace bf
@@ -26,6 +27,8 @@ namespace lua
 
 void addLuaRef( const std::string & str, int ref );
 void removeLuaRef( const std::string & str );
+
+sf::Keyboard::Key getKeyFromString( const char * str );
 
 /***************************************************************************/
 
@@ -106,6 +109,15 @@ static int game_unhook( lua_State * l )
 	return 0;
 }
 
+// game.isKeyPressed( key, ... )
+static int game_isKeyPressed( lua_State * l )
+{
+	int stack = lua_gettop( l );
+	for ( int i = 1; i <= stack; i++ )
+		lua_pushboolean( l, sf::Keyboard::isKeyPressed( getKeyFromString( lua_tostring( l, i ) ) ) );
+	return stack;
+}
+
 // game.newContainer()
 // creates a new drawing container
 static int game_newContainer( lua_State * l )
@@ -168,6 +180,7 @@ static const struct luaL_Reg libgame[] =
 	{ "fadeOut",		game_fadeOut },
 	{ "hook",			game_hook },
 	{ "unhook",		game_unhook },
+	{ "isKeyPressed",	game_isKeyPressed },
 	{ "newContainer",	game_newContainer },
 	{ "newImage", 		game_newImage },
 	{ "newText",		game_newText },
@@ -1215,6 +1228,123 @@ void update( unsigned ms )
 			removeLuaRef( ref.first );
 		}
 	}
+}
+
+/***************************************************************************/
+
+sf::Keyboard::Key getKeyFromString( const char * str )
+{
+	static const struct { const char * str; sf::Keyboard::Key key; } keyList[] =
+	{
+		{ "a",		sf::Keyboard::A },
+		{ "b",		sf::Keyboard::B },
+		{ "c",		sf::Keyboard::C },
+		{ "d",		sf::Keyboard::D },
+		{ "e",		sf::Keyboard::E },
+		{ "f",		sf::Keyboard::F },
+		{ "g",		sf::Keyboard::G },
+		{ "h",		sf::Keyboard::H },
+		{ "i",		sf::Keyboard::I },
+		{ "j",		sf::Keyboard::J },
+		{ "k",		sf::Keyboard::K },
+		{ "l",		sf::Keyboard::L },
+		{ "m",		sf::Keyboard::M },
+		{ "n",		sf::Keyboard::N },
+		{ "o",		sf::Keyboard::O },
+		{ "p",		sf::Keyboard::P },
+		{ "q",		sf::Keyboard::Q },
+		{ "r",		sf::Keyboard::R },
+		{ "s",		sf::Keyboard::S },
+		{ "t",		sf::Keyboard::T },
+		{ "u",		sf::Keyboard::U },
+		{ "v",		sf::Keyboard::V },
+		{ "w",		sf::Keyboard::W },
+		{ "x",		sf::Keyboard::X },
+		{ "y",		sf::Keyboard::Y },
+		{ "z",		sf::Keyboard::Z },
+		{ "num0",		sf::Keyboard::Num0 },
+		{ "num1",		sf::Keyboard::Num1 },
+		{ "num2",		sf::Keyboard::Num2 },
+		{ "num3",		sf::Keyboard::Num3 },
+		{ "num4",		sf::Keyboard::Num4 },
+		{ "num5",		sf::Keyboard::Num5 },
+		{ "num6",		sf::Keyboard::Num6 },
+		{ "num7",		sf::Keyboard::Num7 },
+		{ "num8",		sf::Keyboard::Num8 },
+		{ "num9",		sf::Keyboard::Num9 },
+		{ "escape",	sf::Keyboard::Escape },
+		{ "lcontrol",	sf::Keyboard::LControl },
+		{ "lshift",	sf::Keyboard::LShift },
+		{ "lalt",		sf::Keyboard::LAlt },
+		{ "lsystem",	sf::Keyboard::LSystem },
+		{ "rcontrol",	sf::Keyboard::RControl },
+		{ "rshift",	sf::Keyboard::RShift },
+		{ "ralt",		sf::Keyboard::RAlt },
+		{ "rsystem",	sf::Keyboard::RSystem },
+		{ "menu",		sf::Keyboard::Menu },
+		{ "lbracket",	sf::Keyboard::LBracket },
+		{ "rbracket",	sf::Keyboard::RBracket },
+		{ "semicolon",	sf::Keyboard::SemiColon },
+		{ "comma",	sf::Keyboard::Comma },
+		{ "period",	sf::Keyboard::Period },
+		{ "quote",	sf::Keyboard::Quote },
+		{ "slash",	sf::Keyboard::Slash },
+		{ "backslash",	sf::Keyboard::BackSlash },
+		{ "tilde",	sf::Keyboard::Tilde },
+		{ "equal",	sf::Keyboard::Equal },
+		{ "dash",		sf::Keyboard::Dash },
+		{ "space",	sf::Keyboard::Space },
+		{ "return",	sf::Keyboard::Return },
+		{ "backspace",	sf::Keyboard::BackSpace },
+		{ "tab",		sf::Keyboard::Tab },
+		{ "pageup",	sf::Keyboard::PageUp },
+		{ "pagedown",	sf::Keyboard::PageDown },
+		{ "end",		sf::Keyboard::End },
+		{ "home",		sf::Keyboard::Home },
+		{ "insert",	sf::Keyboard::Insert },
+		{ "delete",	sf::Keyboard::Delete },
+		{ "add",		sf::Keyboard::Add },
+		{ "subtract",	sf::Keyboard::Subtract },
+		{ "multiply",	sf::Keyboard::Multiply },
+		{ "divide",	sf::Keyboard::Divide },
+		{ "left",		sf::Keyboard::Left },
+		{ "right",	sf::Keyboard::Right },
+		{ "up",		sf::Keyboard::Up },
+		{ "down",		sf::Keyboard::Down },
+		{ "numpad0",	sf::Keyboard::Numpad0 },
+		{ "numpad1",	sf::Keyboard::Numpad1 },
+		{ "numpad2",	sf::Keyboard::Numpad2 },
+		{ "numpad3",	sf::Keyboard::Numpad3 },
+		{ "numpad4",	sf::Keyboard::Numpad4 },
+		{ "numpad5",	sf::Keyboard::Numpad5 },
+		{ "numpad6",	sf::Keyboard::Numpad6 },
+		{ "numpad7",	sf::Keyboard::Numpad7 },
+		{ "numpad8",	sf::Keyboard::Numpad8 },
+		{ "numpad9",	sf::Keyboard::Numpad9 },
+		{ "f1",		sf::Keyboard::F1 },
+		{ "f2",		sf::Keyboard::F2 },
+		{ "f3",		sf::Keyboard::F3 },
+		{ "f4",		sf::Keyboard::F4 },
+		{ "f5",		sf::Keyboard::F5 },
+		{ "f6",		sf::Keyboard::F6 },
+		{ "f7",		sf::Keyboard::F7 },
+		{ "f8",		sf::Keyboard::F8 },
+		{ "f9",		sf::Keyboard::F9 },
+		{ "f10",		sf::Keyboard::F10 },
+		{ "f11",		sf::Keyboard::F11 },
+		{ "f12",		sf::Keyboard::F12 },
+		{ "f13",		sf::Keyboard::F13 },
+		{ "f14",		sf::Keyboard::F14 },
+		{ "f15",		sf::Keyboard::F15 },
+		{ "pause",	sf::Keyboard::Pause },
+		{ NULL,		sf::Keyboard::Unknown },
+	};
+	
+	for ( int i = 0; keyList[i].str != NULL; i++ )
+		if ( strcmp( str, keyList[i].str ) == 0 )
+			return keyList[i].key;
+	
+	return sf::Keyboard::Unknown;
 }
 
 /***************************************************************************/
